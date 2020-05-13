c2eb1cf235cd221131aafab4e53039b1e54e1e11
gkaragoz <gkaragoz96@gmail.com>
2020-05-14 02:14:19 +0300

    Try to implement userManager. not ready yet.

--- ChatAppNode/index.js
+++ ChatAppNode/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const User = require('./user');
-const { v4: uuidv4 } = require('uuid');
 
 const strings = require('./strings')
 const Keys = new strings.Keys();
 const Log = require('./Log');
 const RoomManager = require('./roomManager');
+const UserManager = require('./userManager');
 
-let Users = [];
 let roomManager = new RoomManager();
+let userManager = new UserManager();
 let logger = new Log("SERVER");
 
 app.use(express.static('public'));
@@ -21,31 +20,30 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  var user = AddUser(socket.id, socket.rooms);
-  TellServerStatus();
+  var user = userManager.addUser(socket.id, socket.rooms);
+
+  logger.print(user.name + ' named connected to server.');
 
   BroadcastToEveryoneExceptMe(socket, Keys.ON_USER_CONNECTED, user);
 
   socket.on('disconnect', () => {
-    user = RemoveUser(socket.id);
+    user = userManager.removeUser(socket.id);
 
-    logger.print('A user has been disconnected from server: ' + user.name);
+    logger.print(user.name + ' named user has been disconnected from server');
     
-    TellServerStatus();
-
     BroadcastToEveryoneExceptMe(socket, Keys.ON_USER_DISCONNECTED, user);
   });
 
   socket.on(Keys.SEND_USERNAME, (name) => {
     logger.print(socket.id + ' named user has been changed its name to ' + name);
-    user = GetUserById(socket.id);
-    user.name = name;
+    
+    user = userManager.setUsername(socket.id, name);
     
     BroadcastToEveryoneExceptMe(socket, Keys.ON_USERNAME_CHANGED, user);
   });
 
   socket.on(Keys.SEND_JOIN_TO_ROOM, (roomName) => {
-    user = GetUserById(socket.id);
+    user = userManager.getUserById(socket.id);
     logger.print(user.name + " named user wants to join room called " + roomName);
 
     roomManager.addUser(socket, user, roomName, (isSuccess) => {
@@ -58,7 +56,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on(Keys.SEND_LEAVE_FROM_ROOM, (roomName) => {
-    user = GetUserById(socket.id);
+    user = userManager.getUserById(socket.id);
     logger.print(user.name + " named user wants to leave from room called " + roomName);
 
     roomManager.removeUser(socket, user, roomName, (isSuccess) => {
@@ -68,37 +66,8 @@ io.on('connection', (socket) => {
         BroadcastToEveryoneIncludeMe(io, Keys.ON_LEAVE_FROM_ROOM_FAILED);
       }
     });
-
   });
-
 });
-
-function AddUser(id, rooms) {
-  var user = new User(id, uuidv4(), rooms);
-  Users.push(user);
-
-  logger.print('New connection received!');
-
-  return user;
-}
-
-function RemoveUser(id) {
-  var user = GetUserById(id);
-
-  index = Users.indexOf(user);
-
-  Users.splice(index, 1);
-
-  return user;
-}
-
-function GetUserById(id) {
-  return Users.find(user => user.id === id);
-}
-
-function GetPrettyUserPrint(user) {
-  return JSON.stringify(user, null, ' ');
-}
 
 function BroadcastToEveryoneExceptMe(socket, EVENT_CODE, data) {
   socket.broadcast.emit(EVENT_CODE, data);
@@ -106,16 +75,6 @@ function BroadcastToEveryoneExceptMe(socket, EVENT_CODE, data) {
 
 function BroadcastToEveryoneIncludeMe(io, EVENT_CODE, data) {
   io.emit(EVENT_CODE, data);
-}
-
-function TellServerStatus() {
-  logger.print("Online users count: " + Users.length);
-}
-
-function TellUsersDetails() {
-  Users.forEach(user => {
-    logger.print("User: " + GetPrettyUserPrint(user));
-  });
 }
 
 http.listen(3000, () => {
