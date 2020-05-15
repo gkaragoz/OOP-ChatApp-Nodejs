d59db5ffa287e1624a29ee829390d132ad4be310
gkaragoz <gkaragoz96@gmail.com>
2020-05-15 20:52:59 +0300

    Implement datetime and id to message model.

--- ChatAppNode/message.js
+++ ChatAppNode/message.js
@@ -1,8 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
+
 function Message(senderName, receiverName, text, messageType) {
+    this.id = uuidv4();
     this.senderName = senderName;
     this.receiverName = receiverName;
     this.text = text;
     this.messageType = messageType;
+    this.dateTime = Date.now().toLocaleString();
 }
 
 module.exports = Message;
